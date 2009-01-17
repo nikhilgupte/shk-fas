b863c9fd78d87a93bf766b94f877e32522864e84
Nikhil Gupte <nikhilgupte@gmail.com>
2009-01-17 20:37:29 +0530

    yada yada yada

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,13 +3,14 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  before_filter :auto_login
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '3ba0fd10a89e5b999002c6cc1668d310'
+  #protect_from_forgery
   
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+
+  private
+  def auto_login
+    @logged_in_user = User.find(1)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def reqd
+    '<span class="reqd">*</span>'
+  end
+
+  def tip(text)
+    "<span class='tip'>#{text}</span>"
+  end
+
+  def cancel_link(url, label = 'Cancel')
+    link_to label, url, :class => 'cancel_link', :confirm => 'Cancel operation?'
+  end
+
+  def flash_message
+    "<div class='flash_message'><div class='curved'>
+      <a href='#' onclick='return disable_flash_messages();'>&times;#{javascript_tag('setTimeout("disable_flash_messages()", 5000)')}</a>
+      #{flash[:notice]}</div>
+      </div>" if flash[:notice]
+  end
+
 end
