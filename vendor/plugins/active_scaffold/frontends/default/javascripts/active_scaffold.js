36045bc69b70037a4ec507e6f255b018e20a6e72
Nikhil Gupte <nikhilgupte@gmail.com>
2009-02-10 14:03:07 +0530

    ..

--- vendor/plugins/active_scaffold/frontends/default/javascripts/active_scaffold.js
+++ vendor/plugins/active_scaffold/frontends/default/javascripts/active_scaffold.js
@@ -3,9 +3,9 @@ if (typeof Prototype == 'undefined')
   warning = "ActiveScaffold Error: Prototype could not be found. Please make sure that your application's layout includes prototype.js (e.g. <%= javascript_include_tag :defaults %>) *before* it includes active_scaffold.js (e.g. <%= active_scaffold_includes %>).";
   alert(warning);
 }
-if (Prototype.Version.substring(0, 8) == '1.5.0_rc')
+if (Prototype.Version.substring(0, 3) != '1.6')
 {
-  warning = "ActiveScaffold Error: Prototype 1.5.0_rc is not supported. Please update prototype.js (rake rails:update:javascripts).";
+  warning = "ActiveScaffold Error: Prototype version 1.6.x is required. Please update prototype.js (rake rails:update:javascripts).";
   alert(warning);
 }
 
@@ -68,11 +68,13 @@ var ActiveScaffold = {
     });
   },
   decrement_record_count: function(scaffold_id) {
-    count = $$('#' + scaffold_id + ' span.active-scaffold-records').first();
+    // decrement the last record count, firsts record count are in nested lists
+    count = $$('#' + scaffold_id + ' span.active-scaffold-records').last();
     count.innerHTML = parseInt(count.innerHTML) - 1;
   },
   increment_record_count: function(scaffold_id) {
-    count = $$('#' + scaffold_id + ' span.active-scaffold-records').first();
+    // increment the last record count, firsts record count are in nested lists
+    count = $$('#' + scaffold_id + ' span.active-scaffold-records').last();
     count.innerHTML = parseInt(count.innerHTML) + 1;
   },
 
@@ -190,6 +192,12 @@ ActiveScaffold.ActionLink.Abstract.prototype = {
   initialize: function(a, target, loading_indicator) {
     this.tag = $(a);
     this.url = this.tag.href;
+    this.method = 'get';
+    if(this.url.match('_method=delete')){
+      this.method = 'delete';
+    } else if(this.url.match('_method=post')){
+      this.method = 'post';
+    }
     this.target = target;
     this.loading_indicator = loading_indicator;
     this.hide_target = false;
@@ -228,7 +236,7 @@ ActiveScaffold.ActionLink.Abstract.prototype = {
 	    new Ajax.Request(this.url, {
 	      asynchronous: true,
 	      evalScripts: true,
-
+              method: this.method,
 	      onSuccess: function(request) {
 	        if (this.position) {
 	          this.insert(request.responseText);
@@ -314,10 +322,14 @@ ActiveScaffold.Actions.Record.prototype = Object.extend(new ActiveScaffold.Actio
 
 ActiveScaffold.ActionLink.Record = Class.create();
 ActiveScaffold.ActionLink.Record.prototype = Object.extend(new ActiveScaffold.ActionLink.Abstract(), {
-  insert: function(content) {
+  close_previous_adapter: function() {
     this.set.links.each(function(item) {
       if (item.url != this.url && item.is_disabled() && item.adapter) item.close();
     }.bind(this));
+  },
+
+  insert: function(content) {
+    this.close_previous_adapter();
 
     if (this.position == 'replace') {
       this.position = 'after';
@@ -352,7 +364,7 @@ ActiveScaffold.ActionLink.Record.prototype = Object.extend(new ActiveScaffold.Ac
     new Ajax.Request(this.refresh_url, {
       asynchronous: true,
       evalScripts: true,
-
+      method: this.method,
       onSuccess: function(request) {
         Element.replace(this.target, request.responseText);
         var new_target = $(this.target.id);
--- vendor/plugins/active_scaffold/lib/extensions/generic_view_paths.rb
+++ vendor/plugins/active_scaffold/lib/extensions/generic_view_paths.rb
@@ -1,85 +1,68 @@
-# The view_paths functionality in Edge Rails (Rails 2.0) doesn't support
-# the idea of a fallback generic template file, such as what make ActiveScaffold
-# work. This patch adds generic_view_paths, which are folders containing templates
-# that may apply to all controllers.
-#
-# There is one major difference with generic_view_paths, though. They should
-# *not* be used unless the action has been explicitly defined in the controller.
-# This is in contrast to how Rails will normally bypass the controller if it sees
-# a partial.
-
-# if render_action exists, we can use our existing hooks.
-unless ActionController::Base.method_defined? :render_action
-
 class ActionController::Base
   class_inheritable_accessor :generic_view_paths
   self.generic_view_paths = []
-end
 
-# find_full_template_path is no longer around in edge rails. Replaced with the TemplateFinder class (leading
-# to much headache)
-if ActionView::Base.method_defined? :find_full_template_path
-  class ActionView::Base
-    private
-    def find_full_template_path_with_generic_paths(template_path, extension)
-      path = find_full_template_path_without_generic_paths(template_path, extension)
-      if path and not path.empty?
-        path
-      elsif search_generic_view_paths?
-        template_file = File.basename("#{template_path}.#{extension}")
-        path = find_generic_base_path_for(template_file)
-        path ? "#{path}/#{template_file}" : ""
-      else
-        ""
-      end
+  # Returns the view path that contains the given relative template path.
+  def find_generic_base_path_for(template_path, extension)
+    self.generic_view_paths.each do |generic_path|
+      template_file_name = File.basename("#{template_path}.#{extension}")
+      generic_file_path = File.join(generic_path, template_file_name)
+      return generic_file_path if File.file?(generic_file_path)
     end
-    alias_method_chain :find_full_template_path, :generic_paths
-
-    # Returns the view path that contains the given relative template path.
-    def find_generic_base_path_for(template_file_name)
-      controller.generic_view_paths.find { |p| File.file?(File.join(p, template_file_name)) }
-    end
-
-    # We don't want to use generic_view_paths in ActionMailer, and we don't want
-    # to use them unless the controller action was explicitly defined.
-    def search_generic_view_paths?
-      controller.respond_to?(:generic_view_paths) and controller.class.action_methods.include?(controller.action_name)
-    end
-  end
-else
-  # This hooks into edge rails as of revision 8804 (desparately need the new polymorphic eagerloading from edge)
-  class ActionView::TemplateFinder
-    def pick_template_with_generic_paths(template_path, extension)
-      path = pick_template_without_generic_paths(template_path, extension)
-      if path && !path.empty?
-        path
-      else
-        template_file = File.basename(template_path)
-        template_path = find_generic_base_path_for(template_file, extension)
-        # ACC return absolute path to file
-        template_path
-      end
-    end
-    alias_method_chain :pick_template, :generic_paths
-    alias_method :template_exists?, :pick_template
-
-    # Returns the view path that contains the relative template 
-    def find_generic_base_path_for(template_file_name, extension)
-      path = File.expand_path(File.dirname(__FILE__)) + "/../../frontends/default/views"
-      # Should be able to use a rails method here to do this directory search
-      file = Dir.entries(path).find {|f| f =~ /^_?#{template_file_name}\.?#{extension}/ }
-      file ? File.join(path, file) : nil
-    end
-
-    def find_template_extension_from_handler_with_generics(template_path, template_format = @template.template_format)
-      t_ext = find_template_extension_from_handler_without_generics(template_path, template_format)
-      if t_ext && !t_ext.empty?
-        t_ext
-      else
-        'rhtml'
-      end
-    end
-    alias_method_chain :find_template_extension_from_handler, :generics
+    nil
   end
 end
+
+class ActionView::Base
+  def template_exists?(template)
+    begin
+      return _pick_template(template)
+    rescue ActionView::MissingTemplate
+      return nil
+    end
+  end
+
+  private
+  def _pick_template_with_generic(template_path)
+    return template_path if template_path.respond_to?(:render)
+
+    path = template_path.sub(/^\//, '')
+    if m = path.match(/(.*)\.(\w+)$/)
+      template_file_name, template_file_extension = m[1], m[2]
+    else
+      template_file_name = path
+    end
+    if m = template_file_name.match(/\/(\w+)$/)
+      generic_template = m[1]
+    end
+    
+    # OPTIMIZE: Checks to lookup template in view path
+    if template = self.view_paths["#{template_file_name}.#{template_format}"]
+      template
+    elsif template = self.view_paths[template_file_name]
+      template
+    elsif template = self.view_paths[generic_template]
+      template
+    elsif (first_render = @_render_stack.first) && first_render.respond_to?(:format_and_extension) &&
+        (template = self.view_paths["#{template_file_name}.#{first_render.format_and_extension}"])
+      template
+    elsif template_format == :js && template = self.view_paths["#{template_file_name}.html"]
+      @template_format = :html
+      template
+    else
+      template = ActionView::Template.new(template_path, view_paths)
+
+      if self.class.warn_cache_misses && logger
+        logger.debug "[PERFORMANCE] Rendering a template that was " +
+          "not found in view path. Templates outside the view path are " +
+          "not cached and result in expensive disk operations. Move this " +
+          "file into #{view_paths.join(':')} or add the folder to your " +
+          "view path list"
+      end
+
+      template
+    end
+  end
+  alias_method_chain :_pick_template, :generic
+
 end
