38f75ca6b92cab69cb5823b162a6abecdd8140f1
Nikhil Gupte <nikhilgupte@gmail.com>
2009-02-10 14:03:44 +0530

    ..

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Script to install and setup the FAS production environment
+# This script will install ruby, rails and everything required to run the FAS application
+#
+# Dependencies:
+# * Linux installed with gcc and all development libraries
+# * openssl
+# * postgresql and postgresql-devel
+
+
+# install the postgres ruby drivers
+sudo gem install ruby-pg
