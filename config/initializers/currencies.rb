56c7efac005d2f2cef05ec11f2fae450f03d175a
Nikhil Gupte <nikhilgupte@gmail.com>
2009-02-15 21:24:08 +0530

    ..

--- config/initializers/currencies.rb
+++ config/initializers/currencies.rb
@@ -1,7 +1,7 @@
 class Numeric
   %w(usd_in_inr eur_in_inr inr_in_usd inr_in_eur).each do |c|
     define_method c do
-      Currency.send(c, self)
+      Currency.send(c, self) rescue p("Warning: Failed to initialize currencies")
     end
   end
 end
