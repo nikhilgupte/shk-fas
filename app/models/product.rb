f7ff9b4779e5b2c1b29b93c7ea3e81e50bd49150
Nikhil Gupte <nikhilgupte@gmail.com>
2009-03-13 14:20:18 +0530

    enabled product synchronization

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
+require 'dbi'
 class Product < ActiveRecord::Base
 
   validates_presence_of :name, :code
@@ -10,6 +11,31 @@ class Product < ActiveRecord::Base
 
   def long_name
     "#{code} - #{name}"
+  end
+
+  # Synchronizes products from the Business DB (EDPSQL/Business)
+  def self.sync
+    begin
+      dbh = DBI.connect("dbi:ODBC:#{AppConfig.fas_mssql_db_dsn}", AppConfig.fas_mssql_db_username, AppConfig.fas_mssql_db_password)
+      products = dbh.execute('exec sp_BusinessFAS')
+      products.fetch_hash do |s_product|
+        begin
+          if product = find_by_code(s_product['prod_cd'])
+            product.update_attributes :name => s_product['prod_desc'], :quarterly_sales_quantity => s_product['Sales_Qty']
+          else
+            Product.create! :code => s_product['prod_cd'], :name => s_product['prod_desc'], :quarterly_sales_quantity => s_product['Sales_Qty']
+          end
+        rescue
+          logger.error("Error while syncing products: #{s_product['prod_cd']}/#{s_product['prod_desc']}/#{s_product['Sales_Qty']} - #{$!}")
+        end
+      end
+    rescue
+      logger.error("Error while syncing products - #{$!}")
+    end
+  end
+
+  def self.find_by_code(code)
+    find(:first, :conditions => ['lower(code) = ?', code.downcase])
   end
 
   private
--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@
 # * Linux installed with gcc and all development libraries
 # * openssl
 # * postgresql and postgresql-devel
-# * zlib-dev packages
+# * zlib-devel
+# * libreadline-dev
+# * libopenssl-ruby
+# * apache2-prefork-dev
+# * git-core
 
 echo "installing ruby..."
 if [ ! -f ruby-1.8.7-p72.tar.gz ];
@@ -16,6 +20,7 @@ tar -xzf ruby-1.8.7-p72.tar.gz && cd ruby-1.8.7*
 ./configure && make
 sudo make install
 echo 'ruby installed.'
+cd ..
 
 echo "Installing rubygems..."
 if [ ! -f rubygems-1.3.1.tgz ]; then wget http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz ; fi
@@ -24,6 +29,7 @@ cd rubygems-1.3.1
 ruby setup.rb
 cd ..
 echo "rubygems installed!"
+cd ..
 
 echo "installing rails..."
 sudo gem install rails -v 2.2.2
@@ -31,3 +37,9 @@ echo "rails installed!"
 
 # install the postgres ruby drivers
 sudo gem install ruby-pg
+
+gem install passenger
+gem install fastercsv
+gem install calendar_date_select
+
+# install ruby odbc drivers by referring to http://web.archive.org/web/20061011084705/http://wiki.rubyonrails.org/rails/pages/HowtoConnectToMicrosoftSQLServerFromRailsOnLinux
