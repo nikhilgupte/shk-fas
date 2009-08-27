ac1cc02dc6005033f1f23ddbc7b072f99aee50d4
Nikhil Gupte <nikhilgupte@gmail.com>
2009-08-27 10:24:25 +0530

    Added ability to import ingredients.

--- app/controllers/admin/ingredients_controller.rb
+++ app/controllers/admin/ingredients_controller.rb
@@ -1,9 +1,35 @@
 class Admin::IngredientsController < AdminController
   active_scaffold :ingredients do |config|
+    config.action_links.add :import, :label => "Import", :inline => false
     config.actions.exclude :delete, :show
     config.columns.exclude :created_at, :prices
     config.columns[:tax_rate].form_ui = :select
     config.columns[:custom_duty].form_ui = :select
     config.actions.exclude :nested
   end
+
+  def import
+    if request.post?
+      added,updated = 0,0
+      begin
+        FasterCSV.parse(params[:ingredients_file].read.chop, {:headers =>true,:skip_blanks => true}) do |row|
+          code,name = row[0],row[1]
+          unless code.blank? || name.blank?
+            if ingredient= Ingredient.find_by_code(code)
+              ingredient.update_attribute(:name, name)
+              updated += 1
+            else
+              Ingredient.create!(:code => code, :name => name, :import_mode => true)
+              added += 1
+            end
+          end
+        end
+      #rescue
+      end
+      flash[:notice] = "Added #{added} and updated #{updated} ingredients."
+      redirect_to admin_ingredients_path
+    end
+    @title = 'Admin: Ingredients: Import'
+  end
+
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,7 @@
 class Ingredient < ActiveRecord::Base
 
-  validates_presence_of :name, :code, :custom_duty, :tax_rate
+  validates_presence_of :name, :code
+  validates_presence_of :custom_duty, :tax_rate, :unless => Proc.new { |i| i.import_mode }
   validates_uniqueness_of :code, :allow_blank => true
 
   has_many :prices, :class_name => 'IngredientPrice', :order => 'created_at'
@@ -16,12 +17,18 @@ class Ingredient < ActiveRecord::Base
 
   before_validation :fix_fields
 
+  attr_accessor :import_mode
+
   def latest_price
     prices.last
   end
 
   def self.find_by_code(code)
     find(:first, :conditions => ["lower(code) = ?", code.strip.downcase])
+  end
+
+  def has_tax_rate_and_custom_duty?
+    not(tax_rate.nil? || custom_duty.nil?)
   end
 
   private
