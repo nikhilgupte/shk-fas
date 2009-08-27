066d887fbfd5da16b603862a48cebe0c0459d07e
Nikhil Gupte <nikhilgupte@gmail.com>
2009-08-27 10:30:14 +0530

    Added permission for ingredient import

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -16,7 +16,7 @@ class Permission < ActiveRecord::Base
   map_permissions 'orders', :write => [:index, :submit, :create, :edit, :update, :destroy], :export => :export
   map_permissions 'ingredients', :read => [:index, :show], :export => :export
   map_permissions 'ingredients/prices', :write => [:create]
-  map_permissions 'admin/ingredients', :read => [:index, :show], :write => [:edit, :new, :create, :update]
+  map_permissions 'admin/ingredients', :read => [:index, :show], :write => [:edit, :new, :create, :update, :import]
   map_permissions 'admin/formulations', :read => [:index, :show], :write => [:edit, :new, :create, :update, :import]
   map_permissions 'admin/products', :read => [:index, :show], :write => [:edit, :new, :create, :update], :export => :export
   map_permissions 'admin/currencies', :read => [:index, :show], :write => [:edit, :new, :create, :update]
