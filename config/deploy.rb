cfaada889a715a5c15e54ec22f73ae1d5a9f4ac5
Nikhil Gupte <nikhilgupte@gmail.com>
2011-01-03 17:57:53 +0530

    improved deployment routines.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,73 @@
 require 'bundler/capistrano'
-set :application, "fas"
+set :application, "FAS"
 set :repository, "git://github.com/nikhilgupte/shk-fas.git"
-set :use_sudo,    false
-set :scm, :git
+ssh_options[:forward_agent] = true
 set :deploy_via, :remote_cache
-set :deploy_to, "/var/www/apps/#{application}/production"
-set :user,      "fas"
-set :domain,      "210.210.77.180"
-role :app, domain
-role :web, domain
-role :db,  domain, :primary => true
-set :branch, "2.3.5"
+
+set :tag_on_deploy, false
+set :build_gems, false
+set :compress_assets, false
+set :backup_database_before_migrations, false
+set :keep_releases, 5
 
 set :bundle_without, [:development, :test, :cucumber]
 set :bundle_flags,   "--deployment"
 
+default_run_options[:pty] = true
+
+
+after "deploy:update_code", "deploy:link_config"
+after "deploy:update_code", "deploy:generate_css"
+after "deploy:symlink", "deploy:update_crontab"
+
+set :config_files, %w(database.yml)
 namespace :deploy do
-  desc "Update code, disable website, run migrations, enable website"
-  task :full do
-    deploy.web.disable
-    deploy.update
-    deploy.migrate
-    deploy.restart
-    deploy.web.enable
-    deploy.cleanup
+  desc 'symlink config files'
+  task :link_config, :roles => :app do
+    unless config_files.empty?
+      config_files.each do |file|
+        run "ln -nsf #{File.join(shared_path, "config/" + file)} #{File.join(release_path, "/config/" + file)}"
+      end
+    end
+  end
+  
+  desc "Update the crontab file"
+  task :update_crontab, :roles => :db do
+    run "cd #{release_path} && bundle exec whenever --update-crontab #{application} --set environment=#{rails_env}"
   end
 
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
+  desc "Regenerate the CSS files"
+  task :generate_css, :roles => :app do
+    run "cd #{release_path} && rake more:generate RAILS_ENV=#{rails_env}"
   end
-
+  
 end
+
+after "deploy:setup", "thinking_sphinx:shared_sphinx_folder"
+
+after "deploy:start", "bluepill:start"
+after "deploy:stop", "bluepill:quit"
+after "deploy:restart", "bluepill:quit", "bluepill:start"
+
+# Bluepill related tasks
+#after "deploy:update", "bluepill:quit", "bluepill:start"
+namespace :bluepill do
+  desc "Stop processes that bluepill is monitoring and quit bluepill"
+  task :quit, :roles => [:app], :on_error => :continue do
+    sudo "bluepill stop"
+    sleep 10 
+    sudo "bluepill quit"
+    sleep 10 
+  end
+ 
+  desc "Load bluepill configuration and start it"
+  task :start, :roles => [:app] do
+    sudo "bluepill load #{deploy_to}/current/config/#{stage}.pill"
+  end
+ 
+  desc "Prints bluepills monitored processes statuses"
+  task :status, :roles => [:app] do
+    sudo "bluepill status"
+  end
+end
+
