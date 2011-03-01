29af92581e7753b9ebbe35c242d3380b2b108e25
Nikhil Gupte <nikhilgupte@gmail.com>
2011-03-02 02:55:54 +0530

    [#2 state:resolved] urgent- Import Formulation is not working
    Error was cos case sensitive search for code. Fixed.
    Additionally, modified import process to continue even if there are errors and list out the errors with line numbers.

--- app/controllers/admin/formulations_controller.rb
+++ app/controllers/admin/formulations_controller.rb
@@ -8,23 +8,31 @@ class Admin::FormulationsController < AdminController
   def import
     if request.post?
       added,updated = 0,0
+      @errors = []
       begin
         cd = Iconv.new('utf-8', 'iso-8859-1')
+        line_number = 0
         FasterCSV.parse(params[:formulations_file].read.chop, {:headers =>true,:skip_blanks => true}) do |row|
-          code,name = row[0], cd.iconv(row[1])
+          line_number += 1
+          code,name = row[0].strip, cd.iconv(row[1]).strip
           unless code.blank? || name.blank?
             if formulation = Formulation.find_by_code(code)
               formulation.update_attribute(:name, name)
               updated += 1
             else
-              Formulation.create!(:code => code, :name => name)
-              added += 1
+              formulation = nil
+              begin
+                formulation = Formulation.create!(:code => code, :name => name)
+                added += 1
+              rescue
+                @errors << { :line_number => line_number, :message => $!.to_s, :code => code, :name => name }
+              end
             end
           end
         end
       end
       flash[:notice] = "Added #{added} and updated #{updated} formulations."
-      redirect_to admin_formulations_path
+      redirect_to admin_formulations_path unless @errors.present?
     end
     @title = 'Admin: Formulations: Import'
   end
--- app/models/formulation.rb
+++ app/models/formulation.rb
@@ -1,6 +1,14 @@
 class Formulation < ActiveRecord::Base
 
+  validates_uniqueness_of :code, :case_sensitive => false
+
   def name_with_code
     "#{name} (#{code})"
+  end
+
+  class << self
+    def find_by_code(code)
+      first(:conditions => ["lower(code) = ?", code.downcase])
+    end
   end
 end
